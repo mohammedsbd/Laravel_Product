213a258fa554cd6b8968757cd8b03da0b7113941
mohammedsbd <mohammed1112ok@gmail.com>
2026-02-23 03:26:46 -0800

    change middelware wrapping method in routes

--- lara-react-crud/routes/web.php
+++ lara-react-crud/routes/web.php
@@ -10,8 +10,14 @@ Route::get('/', function () {
     ]);
 })->name('home');
 
-Route::get('dashboard', function () {
-    return Inertia::render('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+
+    Route::get('/dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+
+});
+
 
 require __DIR__.'/settings.php';
