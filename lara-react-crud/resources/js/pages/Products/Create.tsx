f5091d07d08c690e7df05582c9497d06e6f9e86f
mohammedsbd <mohammed1112ok@gmail.com>
2026-02-24 03:24:03 -0800

    Update Create.tsx

--- lara-react-crud/resources/js/pages/Products/Create.tsx
+++ lara-react-crud/resources/js/pages/Products/Create.tsx
@@ -4,6 +4,7 @@ import AppLayout from '@/layouts/app-layout';
 import type { BreadcrumbItem } from '@/types';
 import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
 
 
 
@@ -19,12 +20,16 @@ export default function Index() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Create a new product" />
-            <div className="p-4 m-4" >
+            <div className="w-8/12  p-4 m-4" >
                 <form action="">
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                         <div className="col-span-2">
-                            <Label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</Label>
-                            <input type="text" name="name" id="name" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
+                            <Label htmlFor="product name" className="block text-sm font-medium text-gray-700">Name</Label>
+                            <Input placeholder='prodcut name'  type="text" name="name" id="name" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
+                        </div>
+                        <div className="col-span-2">
+                            <Label htmlFor="product price" className="block text-sm font-medium text-gray-700">Name</Label>
+                            <Input placeholder='prodcut price'  type="text" name="name" id="name" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
                         </div>
                     </div>
                 </form>
