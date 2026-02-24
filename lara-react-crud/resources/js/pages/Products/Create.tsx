b586c3a7dba465c06298a16aa7ff1bf5fde6f7a2
mohammedsbd <mohammed1112ok@gmail.com>
2026-02-24 02:46:08 -0800

    Update Create.tsx

---
+++ lara-react-crud/resources/js/pages/Products/Create.tsx
@@ -0,0 +1,30 @@
+import { Head, Link } from '@inertiajs/react';
+
+import AppLayout from '@/layouts/app-layout';
+import type { BreadcrumbItem } from '@/types';
+import { Button } from '@/components/ui/button';
+
+
+
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Products',
+        href: '/products',
+    },
+];
+
+export default function Index() {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Products" />
+            <div className="p-4 m-4" >
+               <Link href={'/products/create'}>
+          <Button>Add Product</Button>
+        </Link>
+
+            </div>
+        </AppLayout>
+    );
+}
+
