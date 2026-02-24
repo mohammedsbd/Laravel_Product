ef9b6c5e094ea483794a57aebf147e5b056287dc
mohammedsbd <mohammed1112ok@gmail.com>
2026-02-24 04:00:18 -0800

    Update Create.tsx

--- lara-react-crud/resources/js/pages/Products/Create.tsx
+++ lara-react-crud/resources/js/pages/Products/Create.tsx
@@ -28,7 +28,7 @@ export default function Index() {
                             <Input placeholder='prodcut name'  type="text" name="name" id="name" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
                         </div>
                         <div className="col-span-2">
-                            <Label htmlFor="product price" className="block text-sm font-medium text-gray-700">Name</Label>
+                            <Label htmlFor="product price" className="block text-sm font-medium text-gray-700">Price</Label>
                             <Input placeholder='prodcut price'  type="text" name="name" id="name" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
                         </div>
                     </div>
