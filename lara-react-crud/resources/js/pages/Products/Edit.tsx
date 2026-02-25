d72dd9d76d076cb2926cc92e37256b334a7813fd
mohammedsbd <mohammed1112ok@gmail.com>
2026-02-25 11:29:12 -0800

    Create Edit.tsx

---
+++ lara-react-crud/resources/js/pages/Products/Edit.tsx
@@ -0,0 +1,79 @@
+import { Head, Link } from '@inertiajs/react';
+import { useForm } from '@inertiajs/react';
+import { CircleAlert } from 'lucide-react';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+
+import AppLayout from '@/layouts/app-layout';
+import { Textarea } from "@/components/ui/textarea"
+import type { BreadcrumbItem } from '@/types';
+import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+;
+
+
+
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Create a new product',
+        href: '/products/create',
+    },
+];
+
+export default function Index() {
+
+  const { data, setData, post, processing, errors } = useForm({
+    name: '',
+    price: '',
+    description: '',
+});
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        post('/products/store');
+        console.log(data);
+    }
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Create a new product" />
+            <div className="w-8/12  p-4 m-4" >
+                <form onSubmit={handleSubmit} action="" className='space-y-4'>
+                    {/* display error */}
+                    {Object.keys(errors).map((key) => (
+                        <Alert key={key}>
+                            <CircleAlert className="h-4 w-4" />
+                            <AlertTitle>{key}</AlertTitle>
+                            <AlertDescription>
+                                            {Object.entries(errors).map(([key, value]) => (
+                                                <p key={key}>{value}</p>
+                                            ))}
+                            </AlertDescription>
+                        </Alert>
+                    ))}
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div className="col-span-2">
+                            <Label htmlFor="product name" className="block text-sm font-medium text-gray-700">Name</Label>
+                            <Input value={data.name} onChange={e => setData('name', e.target.value)} placeholder='prodcut name'  type="text" name="name" id="name" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
+                        </div>
+                        <div className="col-span-2">
+                            <Label htmlFor="product price" className="block text-sm font-medium text-gray-700">Price</Label>
+                            <Input value={data.price} onChange={e => setData('price', e.target.value)} placeholder='prodcut price'  type="text" name="name" id="name" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
+                        </div>
+                        <div className="col-span-2">
+                            <Label htmlFor="product description" className="block text-sm font-medium text-gray-700">Description</Label>
+                           <Textarea value={data.description} onChange={e => setData('description', e.target.value)} placeholder='prodcut description' />
+                           <Button type="submit">Add Product</Button>
+
+                        </div>
+                    </div>
+                </form>
+      
+          {/* <Button>Add Product</Button> */}
+        
+
+            </div>
+        </AppLayout>
+    );
+}
+
