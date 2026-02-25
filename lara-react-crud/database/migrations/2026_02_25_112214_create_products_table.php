a2e491966f589b7ccf53bbc15da3c7e6dd512805
mohammedsbd <mohammed1112ok@gmail.com>
2026-02-25 03:29:44 -0800

    Update 2026_02_25_112214_create_products_table.php

--- lara-react-crud/database/migrations/2026_02_25_112214_create_products_table.php
+++ lara-react-crud/database/migrations/2026_02_25_112214_create_products_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('price');
-            $table->text('description');
+            $table->decimal('price', 10, 2);
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
